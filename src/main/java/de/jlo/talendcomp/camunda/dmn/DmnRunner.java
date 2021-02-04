0cbaa6a5aa260284865f6d02203a1d09e859d546
Jan Lolling <jan.lolling@gmail.com>
2021-02-04 22:15:32 +0100

    First tests DmnRunner has implementation to load the decision and validate in-/output

---
+++ src/main/java/de/jlo/talendcomp/camunda/dmn/DmnRunner.java
@@ -0,0 +1,272 @@
+/**
+ * Copyright 2021 Jan Lolling jan.lolling@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.jlo.talendcomp.camunda.dmn;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.camunda.bpm.dmn.engine.DmnDecision;
+import org.camunda.bpm.dmn.engine.DmnDecisionLogic;
+import org.camunda.bpm.dmn.engine.DmnEngine;
+import org.camunda.bpm.dmn.engine.DmnEngineConfiguration;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableImpl;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableInputImpl;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableOutputImpl;
+import org.camunda.bpm.dmn.engine.impl.DmnExpressionImpl;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+
+/**
+ * Utility to run DMN decisions inside Talend
+ * @author jan.lolling@gmail.com
+ */
+public class DmnRunner {
+	
+	private DmnEngine dmnEngine = null;
+	private DmnDecision decision = null;
+	private VariableMap variables = null;
+	private boolean useCachedDecision = false;
+	private static Map<String, DmnDecision> decisionMap = new HashMap<>();
+	private List<DmnDecisionTableInputImpl> listDecisionTableInputs = null;
+	private List<DmnDecisionTableOutputImpl> listDecisionTableOutputs = null;
+	private List<String> listTalendIncomingColumns = new ArrayList<>();
+	private List<String> listTalendOutgoingColumns = new ArrayList<>();
+	
+	public DmnRunner() {
+		dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+	}
+	
+	/**
+	 * load the DMN rules from a resource
+	 * @param decisionKey the decision key of the final decision table of the DMN
+	 * @param resourceName
+	 * @throws Exception
+	 */
+	public void loadDmnFromResource(String decisionKey, String resourceName) throws Exception {
+		if (isEmpty(decisionKey)) {
+			throw new IllegalArgumentException("decisionKey cannot be null or empty");
+		}
+		if (isEmpty(resourceName)) {
+			throw new IllegalArgumentException("resourceName cannot be null or empty");
+		}
+		if (useCachedDecision) {
+			decision = decisionMap.get(resourceName+"/"+decisionKey);
+		}
+		if (decision == null) {
+			InputStream in = null;
+			try {
+				in = DmnRunner.class.getResourceAsStream(resourceName);
+				if (in == null) {
+					throw new Exception("Resource: " + resourceName + " not available");
+				}
+				decision = dmnEngine.parseDecision(decisionKey, in);
+				if (useCachedDecision) {
+					decisionMap.put(resourceName+":"+decisionKey, decision);
+				}
+				inspectDecisionIO();
+			} catch (Exception e) {
+				throw new Exception("Load decision with key: " + decisionKey + " from resource: " + resourceName + " failed: " + e.getMessage(), e);
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException ioe) {}
+				}
+			}
+		}
+	}
+
+	/**
+	 * load the DMN rules from a file
+	 * @param decisionKey the decision key of the final decision table of the DMN
+	 * @param path the path to the file
+	 * @throws Exception
+	 */
+	public void loadDmnFromFile(String decisionKey, String path) throws Exception {
+		if (isEmpty(decisionKey)) {
+			throw new IllegalArgumentException("decisionKey cannot be null or empty");
+		}
+		if (isEmpty(path)) {
+			throw new IllegalArgumentException("path cannot be null or empty");
+		}
+		if (useCachedDecision) {
+			decision = decisionMap.get(path+"/"+decisionKey);
+		}
+		if (decision == null) {
+			InputStream in = null;
+			try {
+				File test = new File(path);
+				if (test.canRead() == false) {
+					throw new Exception("File: " + test.getAbsolutePath() + " does not exist or cannot be read");
+				}
+				in = Files.newInputStream(Paths.get(path), StandardOpenOption.READ);
+				if (in == null) {
+					throw new Exception("File with path: " + path + " cannot be read");
+				}
+				decision = dmnEngine.parseDecision(decisionKey, in);
+				if (useCachedDecision) {
+					decisionMap.put(path+":"+decisionKey, decision);
+				}
+				inspectDecisionIO();
+			} catch (Exception e) {
+				throw new Exception("Load decision with key: " + decisionKey + " from path: " + path + " failed: " + e.getMessage(), e);
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException ioe) {}
+				}
+			}
+		}
+	}
+	
+	private void inspectDecisionIO() {
+		if (decision == null) {
+			throw new IllegalStateException("Decision not loaded");
+		}
+		DmnDecisionLogic l = decision.getDecisionLogic();
+		if (l instanceof DmnDecisionTableImpl) {
+			DmnDecisionTableImpl dti = (DmnDecisionTableImpl) l;
+			listDecisionTableInputs = dti.getInputs();
+			listDecisionTableOutputs = dti.getOutputs();
+		}
+	}
+	
+	public DmnDecision getDecision() {
+		return decision;
+	}
+	
+	/**
+	 * clears the former values. Must be performed before loading a new schema record
+	 */
+	public void clearVariables() {
+		if (variables != null) {
+			variables.clear();
+			variables = null;		
+		}
+	}
+
+	/**
+	 * adds a new input value
+	 * @param variableName
+	 * @param value
+	 */
+	public void addInputValue(String variableName, Object value) {
+		if (isEmpty(variableName)) {
+			throw new IllegalArgumentException("variableName cannot be null or empty");
+		}
+		if (variables == null) {
+			variables = Variables.putValue(variableName, value);
+		} else {
+			variables.putValue(variableName, value);
+		}
+	}
+	
+	/**
+	 * checks if the input variables from the decision have corresponding incoming schema columns
+	 * @throws Exception if it is not the case
+	 */
+	public void validateInputVariables() throws Exception {
+		if (listTalendIncomingColumns == null) {
+			throw new Exception("No Talend schema variables as input are set!");
+		}
+		if (listDecisionTableInputs != null) {
+			StringBuilder sb = new StringBuilder();
+			for (DmnDecisionTableInputImpl input : listDecisionTableInputs) {
+				String key = null;
+				DmnExpressionImpl ex = input.getExpression();
+				if (ex != null) {
+					key = ex.getExpression();
+				}
+				if (isEmpty(key)) {
+					key = input.getInputVariable();
+				}
+				if (listTalendIncomingColumns.contains(key) == false) {
+					sb.append("Decisions input: " + key + " has no corresponding Talend incoming schema column\n");
+				}
+			}
+			if (sb.length() > 0) {
+				throw new Exception(sb.toString());
+			}
+		}
+	}
+	
+	/**
+	 * add a Talend schema output variable for the validation for later validation
+	 * @param schemaOutputColumn
+	 */
+	public void addExpectedOutputVariable(String schemaOutputColumn) {
+		if (isEmpty(schemaOutputColumn)) {
+			throw new IllegalArgumentException("schemaOutputColumn cannot be null or empty");
+		}
+		listTalendOutgoingColumns.add(schemaOutputColumn);
+	}
+	
+	/**
+	 * checks if the Talend output variables have a corresponding output variable in the decision
+	 * @throws Exception if it is not the case
+	 */
+	public void validateOutputVariables() throws Exception {
+		if (listTalendOutgoingColumns == null) {
+			throw new Exception("No Talend schema variables as output are set!");
+		}
+		if (listDecisionTableOutputs != null) {
+			StringBuilder sb = new StringBuilder();
+			for (String name : listTalendOutgoingColumns) {
+				boolean exists = false;
+				for (DmnDecisionTableOutputImpl output : listDecisionTableOutputs) {
+					String key = output.getOutputName();
+					if (name.equals(key)) {
+						exists = true;
+					}
+				}
+				if (exists == false) {
+					sb.append("Talend outgoing schema column: " + name + " has no output variable within the decsion\n");
+				}
+			}
+			if (sb.length() > 0) {
+				throw new Exception(sb.toString());
+			}
+		}
+	}
+	
+	/**
+	 * returns true if the string is null or empty or equals "null"
+	 * @param s the string
+	 * @returns true if empty 
+	 */
+	public static boolean isEmpty(String s) {
+		if (s == null) {
+			return true;
+		}
+		if (s.trim().isEmpty()) {
+			return true;
+		}
+		if (s.trim().equalsIgnoreCase("null")) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/test/java/de/jlo/talendcomp/camunda/dmn/TestDMNEngine.java
@@ -0,0 +1,234 @@
+package de.jlo.talendcomp.camunda.dmn;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.camunda.bpm.dmn.engine.DmnDecision;
+import org.camunda.bpm.dmn.engine.DmnDecisionLogic;
+import org.camunda.bpm.dmn.engine.DmnDecisionResult;
+import org.camunda.bpm.dmn.engine.DmnDecisionResultEntries;
+import org.camunda.bpm.dmn.engine.DmnDecisionRuleResult;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.dmn.engine.DmnEngine;
+import org.camunda.bpm.dmn.engine.DmnEngineConfiguration;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableImpl;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableInputImpl;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableOutputImpl;
+import org.camunda.bpm.dmn.engine.spi.DmnEngineMetricCollector;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.junit.Test;
+
+public class TestDMNEngine {
+
+	@Test
+	public void testFirstEngineExample() throws Exception {
+		String season = "Winter";
+		int guestCount = 2;
+		VariableMap variables = Variables.putValue("season", season).putValue("guestCount", guestCount);
+
+		DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+
+		InputStream inputStream = TestDMNEngine.class.getResourceAsStream("/dish-decision.dmn11.dmn");
+
+		try {
+			DmnDecision decision = dmnEngine.parseDecision("decision", inputStream);
+
+			// evaluate decision
+			DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
+
+			// print result
+			String desiredDish = result.getSingleResult().getSingleEntry();
+			System.out.println("Dish Decision:\n\tI would recommend to serve: " + desiredDish);
+
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				System.err.println("Could not close stream: " + e.getMessage());
+			}
+		}
+	}
+
+	@Test
+	public void testReturnResults() throws Exception {
+		String season = null; //"Winter";
+		int guestCount = 2;
+		Map<String, Object> variables = Variables.putValue("guestCount", guestCount).putValue("season", season);
+
+		DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+
+		InputStream inputStream = TestDMNEngine.class.getResourceAsStream("/dish-decision.dmn11.dmn");
+
+		try {
+			DmnDecision decision = dmnEngine.parseDecision("decision", inputStream);
+			DmnDecisionLogic l = decision.getDecisionLogic();
+			if (l instanceof DmnDecisionTableImpl) {
+				DmnDecisionTableImpl dti = (DmnDecisionTableImpl) l;
+				List<DmnDecisionTableInputImpl> listInputs = dti.getInputs();
+				for (DmnDecisionTableInputImpl impl : listInputs) {
+					System.out.println("input var: " + impl.getInputVariable());
+					System.out.println("input name: " + impl.getName());
+					System.out.println("input id: " + impl.getId());
+					System.out.println("input expression: " + impl.getExpression().getExpression());
+					System.out.println("input type: " + impl.getExpression().getTypeDefinition().getTypeName());
+				}
+				List<DmnDecisionTableOutputImpl> listOutputs = dti.getOutputs();
+				for (DmnDecisionTableOutputImpl impl : listOutputs) {
+					System.out.println("output var: " + impl.getOutputName());
+					System.out.println("output name: " + impl.getName());
+					System.out.println("output id: " + impl.getId());
+					System.out.println("output type: " + impl.getTypeDefinition().getTypeName());
+				}
+			}
+			// evaluate decision
+			DmnDecisionTableResult result = dmnEngine.evaluateDecisionTable(decision, variables);
+
+			for (DmnDecisionRuleResult oneResult : result) {
+				for (Map.Entry<String, Object> oneValue : oneResult.entrySet()) {
+					String var = oneValue.getKey();
+					Object value = oneValue.getValue();
+					System.out.println(var + "=" + value);
+				}
+			}
+			
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
+
+	@Test
+	public void testReturnDRG() throws Exception {
+		String season = "Summer";
+		int guestCount = 2;
+		VariableMap variables = Variables.putValue("season", season).putValue("guestCount", guestCount);
+
+		DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+		DmnEngineMetricCollector collector = dmnEngine.getConfiguration().getEngineMetricCollector();
+
+		InputStream inputStream = TestDMNEngine.class.getResourceAsStream("/dish-decision.dmn11.dmn");
+
+		try {
+			DmnDecision decision = dmnEngine.parseDecision("decision", inputStream);
+
+			// evaluate decision
+			DmnDecisionResult result = dmnEngine.evaluateDecision(decision, variables);
+
+			for (DmnDecisionResultEntries oneResult : result) {
+				for (Map.Entry<String, Object> oneValue : oneResult.entrySet()) {
+					String var = oneValue.getKey();
+					Object value = oneValue.getValue();
+					System.out.println(var + "=" + value);
+				}
+			}
+			System.out.println("executed elements=" + collector.getExecutedDecisionElements());
+			System.out.println("executed instances=" + collector.getExecutedDecisionInstances());
+			
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
+	
+	@Test
+	public void testReturnMultiRow() throws Exception {
+		String product = "Product1";
+		String type = "1";
+		String grade = "4307";
+		int width = 2000;
+		VariableMap variables = Variables
+								.putValue("product", product)
+								.putValue("type", type)
+								.putValue("grade", grade)
+								.putValue("width", width);
+
+		DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+		DmnEngineMetricCollector collector = dmnEngine.getConfiguration().getEngineMetricCollector();
+
+		InputStream inputStream = TestDMNEngine.class.getResourceAsStream("/collect_sum_example.dmn");
+
+		try {
+			DmnDecision decision = dmnEngine.parseDecision("surcharge", inputStream);
+			System.out.println("decision-key=" + decision.getKey());
+			System.out.println("isDecisionTable=" + decision.isDecisionTable());
+			
+			// evaluate decision
+			DmnDecisionResult result = dmnEngine.evaluateDecision(decision, variables);
+
+			for (DmnDecisionResultEntries oneResult : result) {
+				System.out.println("----------------------");
+				for (Map.Entry<String, Object> oneValue : oneResult.entrySet()) {
+					String var = oneValue.getKey();
+					Object value = oneValue.getValue();
+					System.out.println(var + "=" + value);
+				}
+			}
+			System.out.println("==========================");
+			System.out.println("executed elements=" + collector.getExecutedDecisionElements());
+			System.out.println("executed instances=" + collector.getExecutedDecisionInstances());
+			assertEquals(2, result.size());
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
+
+	@Test
+	public void testDRG() throws Exception {
+		String season = "Winter";
+		int guestCount = 3;
+		boolean guestsWithChildren = false;
+
+		VariableMap variables = Variables
+								.putValue("season", season)
+								.putValue("guestCount", guestCount)
+								.putValue("guestsWithChildren", guestsWithChildren);
+
+		DmnEngine dmnEngine = DmnEngineConfiguration.createDefaultDmnEngineConfiguration().buildEngine();
+		DmnEngineMetricCollector collector = dmnEngine.getConfiguration().getEngineMetricCollector();
+
+		InputStream inputStream = TestDMNEngine.class.getResourceAsStream("/drg_dishes.dmn");
+
+		try {
+			DmnDecision decision = dmnEngine.parseDecision("beverages", inputStream);
+			System.out.println("drg-key=" + decision.getKey());
+			
+			// evaluate decision
+			DmnDecisionResult result = dmnEngine.evaluateDecision(decision, variables);
+
+			for (DmnDecisionResultEntries oneResult : result) {
+				System.out.println("----------------------");
+				for (Map.Entry<String, Object> oneValue : oneResult.entrySet()) {
+					String var = oneValue.getKey();
+					Object value = oneValue.getValue();
+					System.out.println(var + "=" + value);
+				}
+			}
+			System.out.println("==========================");
+			System.out.println("executed elements=" + collector.getExecutedDecisionElements());
+			System.out.println("executed instances=" + collector.getExecutedDecisionInstances());
+			assertEquals(2, result.size());
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+	}
+
+}
